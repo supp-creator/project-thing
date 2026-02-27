927d7f75c4dc48461edbeec3548c5c8d9ff43e69
supp <tyronesarmiento3@gmail.com>
2026-02-27 13:32:29 +0800

    Started work on command line parsing functions.

--- src/clt/main.py
+++ src/clt/main.py
@@ -1,3 +1,40 @@
-from cryptography import *
+import cryptography
+import argparse
 
-def main()
+
+# Once class per custom Action for text parsing.
+class NewFileAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+
+class OpenFileAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+
+class PasswordAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+
+class DeleteAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+
+def main():
+    #Parses inputs into executable commands for the program.
+    parser = argparse.ArgumentParser(prog="NOTES")
+
+    parse.add_argument("--quit", action="store_true")
+    parse.add_argument("--new", action=NewFileAction, help="Creates a new encrypted file.")
+    parse.add_argument("--open", action=OpenFileAction, help="Decrypts and opens file.")
+    parse.add_argument("--login", action=PasswordAction, help="Authenticates user.")
+    parse.add_argument("--delete", action=DeleteAction, help="Deletes specified file.")
+    parse.add_argument("--help", action="help")
+
+    args = parser.parse_args()
+
+    state = standby
+
+    while state = standby:
+    
+
+
+
+
+if __name__== "__main__":
+    main()
